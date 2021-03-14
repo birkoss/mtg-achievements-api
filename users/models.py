4e5fc096108c9de5e1a896f9507fc85b70b39180
Mathieu Robichaud <admin@birkoss.com>
2021-03-14 08:37:46 -0400

    Added Rest Framework and User Token

--- users/models.py
+++ users/models.py
@@ -1,5 +1,9 @@
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from rest_framework.authtoken.models import Token
 
 from core.models import TimeStampedModel, UUIDModel
 
@@ -25,3 +29,10 @@ class User(PermissionsMixin, UUIDModel, TimeStampedModel, AbstractBaseUser):
 
     def __str__(self):
         return self.email
+
+
+# Create an API token when an user is created
+@receiver(post_save, sender=User)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
